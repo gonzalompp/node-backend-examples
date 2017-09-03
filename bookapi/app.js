d3fa226fef1a4037be2fb3192982b83c64f60835
Gonzalo <gonzalompp@gmail.com>
2017-09-03 19:16:54 -0300

    Added Docker-compose config for starting the MongoDB and Node backend

--- bookapi/app.js
+++ bookapi/app.js
@@ -5,6 +5,21 @@ var app = express();
 //environment config
 var port = process.env.PORT || 3000;
 
+var router = express.Router();
+
+router.route('/Books')
+	.get(function(req,res) {
+		var responseJson = {
+			hello: 'world 2'
+		};
+
+		res.json(responseJson);
+	});
+
+
+app.use('/api',router);
+
+
 app.get('/', function(req, res) {
 	res.send('Welcome to the Backend API');
 });
